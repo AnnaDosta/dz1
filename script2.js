bf957d375c12c2df67fe7eddc343904f76a49fd6
dmitry.dosta <dima.dosta9@gmail.com>
2022-05-20 16:47:29 +0300

    dz1

---
+++ script2.js
@@ -0,0 +1,30 @@
+//3 задание
+
+let width = 10;
+let height = 23;
+let SPryam = width * height;
+
+console.log(SPryam);
+
+//6 задание
+
+let aa = 5;
+let bb = 7;
+let h1 = 10;
+let STrap = ((aa + bb) / 2) * h1;
+console.log(STrap);
+//7 задание
+
+let s = 2000000;
+let pro = 0.1 * s; //процент в год
+let years = 5;
+let Pereplata = pro * years;
+console.log(Pereplata);
+
+//8 задание
+let a = 8;
+let b = 3;
+let x1 = (16 + 2 * b - a) / 2;
+let x2 = (a - 15 * b) / (b - 6);
+let x3 = 23780 / (1 + 2 + a + b);
+console.log(x1, x2, x3);
